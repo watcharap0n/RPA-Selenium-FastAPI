a382c986f796e820dbc672c9fc1c0b9b7037b876
watcharap0n <wera.watcharapon@gmail.com>
2021-06-24 15:57:10 +0700

    okja

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-worker: python app.py
+web: uvicorn app:app --host 0.0.0.0 --port $PORT --workers 2
--- app.py
+++ app.py
@@ -1,13 +1,25 @@
 from selenium import webdriver
+from fastapi import FastAPI
+import uvicorn
 import os
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
-chrome_options.binary_location = os.environ["GOOGLE_CHROME_BIN"]
-driver = webdriver.Chrome(executable_path=os.environ["CHROMEDRIVER_PATH"], chrome_options=chrome_options)
+app = FastAPI()
 
-driver.get("https://medium.com")
-print(driver.page_source)
-print("Finished!")
+
+@app.get('/test')
+async def test():
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.binary_location = os.environ["GOOGLE_CHROME_BIN"]
+    driver = webdriver.Chrome(executable_path=os.environ["CHROMEDRIVER_PATH"], chrome_options=chrome_options)
+
+    await driver.get("https://medium.com")
+    print(driver.page_source)
+    print("Finished!")
+    return {'msg': 'success'}
+
+
+if __name__ == '__main__':
+    uvicorn.run('app:app', port=8080, debug=True, host='0.0.0.0')
